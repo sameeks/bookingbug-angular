a5069b44e7d8bf69f88068938e02c57ca053ed4d
Thomas Schneider <ts@thomasschneider.com>
2016-04-12 16:37:08 +0100

    Fixes

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -1,7 +1,7 @@
 exports.config = {
   seleniumServerJar: '../node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-2.53.0.jar',
   specs: ['booking.js.coffee'],
-  capatibilities: {
+  capabilities: {
     browserName: 'chrome',
     'chrome-switches': ['--no-sandbox'] 
   }
