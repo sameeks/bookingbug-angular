7bfb28b2046602bbb30a9d9216470fb15e4c8d7c
damian pudlo <pudlo.damian@gmail.com>
2016-07-15 15:56:21 +0100

    [PLAT-68] run e2e on master and dev branches

--- travis/script.sh
+++ travis/script.sh
@@ -4,6 +4,10 @@ echo "TRAVIS BRANCH: $TRAVIS_BRANCH"
 
 gulp test:unit-ci # !!! generates lcov reports per module - can be used with Climate
 
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_BRANCH" = 'dev' ] ; then
+  gulp test:e2e
+fi
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] ; then
   gulp buildWidget
 fi
