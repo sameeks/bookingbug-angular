f15eb3e0a141322c5535a586d39d47a477c16cd0
damian pudlo <pudlo.damian@gmail.com>
2016-07-27 15:04:18 +0100

    [TD-21] AdminDashboard AdminCoreOptions provider unit tests

--- gulp-tasks/config/other.js
+++ gulp-tasks/config/other.js
@@ -157,8 +157,11 @@
         scripts: [
           'examples/booking-widget.js'
         ]
-      }
-      return gulp.src('src/*/javascripts/**')
+      };
+      return gulp.src([
+        './src/*/javascripts/**',
+        '!./src/*/javascripts/*.spec.js.coffee'
+      ])
         .pipe(gulpif(/.*coffee$/, coffee().on('error', gutil.log)))
         .pipe(gulpDocs.process(options))
         .pipe(gulp.dest('./docs'));
