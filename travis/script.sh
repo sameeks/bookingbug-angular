45a47682b583d31b591800ed88980e5a62289cd6
Ed Clements <ed@bookingbug.com>
2016-07-28 16:55:54 +0100

    Adds build tasks

--- travis/script.sh
+++ travis/script.sh
@@ -9,5 +9,5 @@ echo "TRAVIS BRANCH: $TRAVIS_BRANCH"
 gulp test:unit-ci # !!! generates lcov reports per module - can be used with Climate
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] ; then
-  gulp buildWidget
+  gulp build:widget
 fi
