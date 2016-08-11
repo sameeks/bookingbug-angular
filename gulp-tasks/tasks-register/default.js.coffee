c772f5826ca0f5b089d77c238c89d553fe0ba19a
damian pudlo <pudlo.damian@gmail.com>
2016-08-11 15:16:10 +0100

    [PLAT-68] gulp tasks readme

--- gulp-tasks/tasks-register/default.js.coffee
+++ gulp-tasks/tasks-register/default.js.coffee
@@ -2,4 +2,4 @@ module.exports = (gulp, plugins, path)->
   gulpConnect = require('gulp-connect')
   args = require('../helpers/args.js')
 
-  gulp.task 'default', ['run-project']
+  gulp.task 'default', ['run-project:watch']
--- travis/script.sh
+++ travis/script.sh
@@ -5,6 +5,7 @@ set -ev
 # The -v flag makes the shell print all lines in the script before executing them, which helps identify which steps failed.
 
 echo "TRAVIS BRANCH: $TRAVIS_BRANCH"
+echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 
 gulp test-unit # !!! Generates html & lcov reports. Lcov can be easily used with Climate or Coverall
 
