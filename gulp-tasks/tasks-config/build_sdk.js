5a1d0d28aa545797a18cf71f0b031e49d1e5d0d2
damian pudlo <pudlo.damian@gmail.com>
2016-08-28 13:35:16 +0100

    bb-gulp -> build_sdk

--- gulp-tasks/tasks-config/build_sdk.js
+++ gulp-tasks/tasks-config/build_sdk.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var bbGulp = require('../helpers/bb-gulp.js');
+    var buildSdk = require('../helpers/build_sdk.js');
     var path = require('path');
 
     module.exports = function (gulp, configuration) {
@@ -10,182 +10,182 @@
         var destPath = path.join(configuration.rootPath, 'build');
 
         gulp.task('build-sdk:admin:javascripts', function () {
-            return bbGulp.javascripts('admin', srcPath, destPath, configuration.uglify);
+            return buildSdk.javascripts('admin', srcPath, destPath, configuration.uglify);
         });
 
         gulp.task('build-sdk:admin:images', function () {
-            return bbGulp.images('admin', srcPath, destPath);
+            return buildSdk.images('admin', srcPath, destPath);
         });
 
         gulp.task('build-sdk:admin:templates', function () {
-            return bbGulp.templates('admin', srcPath, destPath);
+            return buildSdk.templates('admin', srcPath, destPath);
         });
 
         gulp.task('build-sdk:admin:bower', function () {
-            return bbGulp.bower('admin', srcPath, destPath);
+            return buildSdk.bower('admin', srcPath, destPath);
         });
 
         //***
 
         gulp.task('build-sdk:admin-booking:javascripts', function () {
-            bbGulp.javascripts('admin-booking', srcPath, destPath, configuration.uglify);
+            buildSdk.javascripts('admin-booking', srcPath, destPath, configuration.uglify);
         });
 
         gulp.task('build-sdk:admin-booking:stylesheets', function () {
-            return bbGulp.stylesheets('admin-booking', srcPath, destPath);
+            return buildSdk.stylesheets('admin-booking', srcPath, destPath);
         });
 
         gulp.task('build-sdk:admin-booking:templates', function () {
-            return bbGulp.templates('admin-booking', srcPath, destPath, 'BBAdminBooking');
+            return buildSdk.templates('admin-booking', srcPath, destPath, 'BBAdminBooking');
         });
 
         gulp.task('build-sdk:admin-booking:bower', function () {
-            return bbGulp.bower('admin-booking', srcPath, destPath, 'BBAdminBooking');
+            return buildSdk.bower('admin-booking', srcPath, destPath, 'BBAdminBooking');
         });
 
         //***
 
         gulp.task('build-sdk:admin-dashboard:javascripts-core', function () {
-            return bbGulp.javascriptsCore('admin-dashboard', srcPath, destPath, 'BBAdminDashboard', configuration.uglify);
+            return buildSdk.javascriptsCore('admin-dashboard', srcPath, destPath, 'BBAdminDashboard', configuration.uglify);
         });
 
         gulp.task('build-sdk:admin-dashboard:javascripts-lazy', function () {
-            return bbGulp.javascriptsLazy('admin-dashboard', srcPath, destPath, 'BBAdminDashboard', configuration.uglify);
+            return buildSdk.javascriptsLazy('admin-dashboard', srcPath, destPath, 'BBAdminDashboard', configuration.uglify);
         });
 
         gulp.task('build-sdk:admin-dashboard:stylesheets', function () {
-            return bbGulp.stylesheets('admin-dashboard', srcPath, destPath);
+            return buildSdk.stylesheets('admin-dashboard', srcPath, destPath);
         });
 
         gulp.task('build-sdk:admin-dashboard:images', function () {
-            return bbGulp.images('admin-dashboard', srcPath, destPath);
+            return buildSdk.images('admin-dashboard', srcPath, destPath);
         });
 
         gulp.task('build-sdk:admin-dashboard:bower', function () {
-            return bbGulp.bower('admin-dashboard', srcPath, destPath);
+            return buildSdk.bower('admin-dashboard', srcPath, destPath);
         });
 
         //***
 
 
         gulp.task('build-sdk:core:javascripts', function () {
-            return bbGulp.javascripts('core', srcPath, destPath, configuration.uglify);
+            return buildSdk.javascripts('core', srcPath, destPath, configuration.uglify);
         });
 
         gulp.task('build-sdk:core:stylesheets', function () {
-            return bbGulp.stylesheets('core', srcPath, destPath);
+            return buildSdk.stylesheets('core', srcPath, destPath);
         });
 
         gulp.task('build-sdk:core:templates', function () {
-            return bbGulp.templates('core', srcPath, destPath);
+            return buildSdk.templates('core', srcPath, destPath);
         });
 
         gulp.task('build-sdk:core:bower', function () {
-            return bbGulp.bower('core', srcPath, destPath);
+            return buildSdk.bower('core', srcPath, destPath);
         });
 
         //***
 
         gulp.task('build-sdk:events:javascripts', function () {
-            return bbGulp.javascripts('events', srcPath, destPath, configuration.uglify);
+            return buildSdk.javascripts('events', srcPath, destPath, configuration.uglify);
         });
 
         gulp.task('build-sdk:events:templates', function () {
-            return bbGulp.templates('events', srcPath, destPath);
+            return buildSdk.templates('events', srcPath, destPath);
         });
 
         gulp.task('build-sdk:events:bower', function () {
-            return bbGulp.bower('events', srcPath, destPath);
+            return buildSdk.bower('events', srcPath, destPath);
         });
 
         //***
 
         gulp.task('build-sdk:member:javascripts', function () {
-            return bbGulp.javascripts('member', srcPath, destPath, configuration.uglify);
+            return buildSdk.javascripts('member', srcPath, destPath, configuration.uglify);
         });
 
         gulp.task('build-sdk:member:stylesheets', function () {
-            return bbGulp.stylesheets('member', srcPath, destPath);
+            return buildSdk.stylesheets('member', srcPath, destPath);
         });
 
         gulp.task('build-sdk:member:templates', function () {
-            return bbGulp.templates('member', srcPath, destPath);
+            return buildSdk.templates('member', srcPath, destPath);
         });
 
         gulp.task('build-sdk:member:bower', function () {
-            return bbGulp.bower('member', srcPath, destPath);
+            return buildSdk.bower('member', srcPath, destPath);
         });
 
         //***
 
         gulp.task('build-sdk:public-booking:javascripts', function () {
-            return bbGulp.javascripts('public-booking', srcPath, destPath, configuration.uglify);
+            return buildSdk.javascripts('public-booking', srcPath, destPath, configuration.uglify);
         });
 
         gulp.task('build-sdk:public-booking:stylesheets', function () {
-            return bbGulp.stylesheets('public-booking', srcPath, destPath);
+            return buildSdk.stylesheets('public-booking', srcPath, destPath);
         });
 
         gulp.task('build-sdk:public-booking:fonts', function () {
-            return bbGulp.fonts('public-booking', srcPath, destPath);
+            return buildSdk.fonts('public-booking', srcPath, destPath);
         });
 
         gulp.task('build-sdk:public-booking:images', function () {
-            return bbGulp.images('public-booking', srcPath, destPath);
+            return buildSdk.images('public-booking', srcPath, destPath);
         });
 
         gulp.task('build-sdk:public-booking:templates', function () {
-            return bbGulp.templates('public-booking', srcPath, destPath);
+            return buildSdk.templates('public-booking', srcPath, destPath);
         });
 
         gulp.task('build-sdk:public-booking:bower', function () {
-            return bbGulp.bower('public-booking', srcPath, destPath);
+            return buildSdk.bower('public-booking', srcPath, destPath);
         });
 
         //***
 
         gulp.task('build-sdk:queue:javascripts', function () {
-            return bbGulp.javascripts('queue', srcPath, destPath, configuration.uglify);
+            return buildSdk.javascripts('queue', srcPath, destPath, configuration.uglify);
         });
 
         gulp.task('build-sdk:queue:templates', function () {
-            return bbGulp.templates('queue', srcPath, destPath);
+            return buildSdk.templates('queue', srcPath, destPath);
         });
 
         gulp.task('build-sdk:queue:bower', function () {
-            return bbGulp.bower('queue', srcPath, destPath);
+            return buildSdk.bower('queue', srcPath, destPath);
         });
 
         //***
 
         gulp.task('build-sdk:services:javascripts', function () {
-            return bbGulp.javascripts('services', srcPath, destPath, configuration.uglify);
+            return buildSdk.javascripts('services', srcPath, destPath, configuration.uglify);
         });
 
         gulp.task('build-sdk:services:stylesheets', function () {
-            return bbGulp.stylesheets('services', srcPath, destPath);
+            return buildSdk.stylesheets('services', srcPath, destPath);
         });
 
         gulp.task('build-sdk:services:templates', function () {
-            return bbGulp.templates('services', srcPath, destPath);
+            return buildSdk.templates('services', srcPath, destPath);
         });
 
         gulp.task('build-sdk:services:bower', function () {
-            return bbGulp.bower('services', srcPath, destPath);
+            return buildSdk.bower('services', srcPath, destPath);
         });
 
         //***
 
         gulp.task('build-sdk:settings:javascripts', function () {
-            return bbGulp.javascripts('settings', srcPath, destPath, configuration.uglify);
+            return buildSdk.javascripts('settings', srcPath, destPath, configuration.uglify);
         });
 
         gulp.task('build-sdk:settings:templates', function () {
-            return bbGulp.templates('settings', srcPath, destPath);
+            return buildSdk.templates('settings', srcPath, destPath);
         });
 
         gulp.task('build-sdk:settings:bower', function () {
-            return bbGulp.bower('settings', srcPath, destPath);
+            return buildSdk.bower('settings', srcPath, destPath);
         });
 
     };
