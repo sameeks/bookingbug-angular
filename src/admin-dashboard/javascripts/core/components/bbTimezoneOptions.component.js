63fdc3f44da32a4c3c1614c58c054c58a61022da
Carlos Rios <crios@bookingbug.com>
2017-02-08 16:09:24 +0000

    coffee to js

---
+++ src/admin-dashboard/javascripts/core/components/bbTimezoneOptions.component.js
@@ -0,0 +1,94 @@
+(function () {
+
+    'use strict';
+
+    /**
+     * @ngdoc component
+     * @name BBAdminDashboard.timezoneOptions
+     *
+     * @description
+     * Timezone options
+     *
+     * @example
+     * <example>
+     *   <timezone-options restrict-region="'Europe'"></timezone-options>
+     *   <timezone-options restrict-region="['Asia', 'America']"></timezone-options>
+     * </example>
+     */
+
+    var timezoneOptionsComponent = {
+        templateUrl: 'core/_bb-timezone-options.html',
+        bindings: {
+            restrictRegion: '<'
+        },
+        controller: TimezoneOptionsCtrl,
+        controllerAs: '$bbTimezoneOptionsCtrl'
+    };
+
+    function TimezoneOptionsCtrl ($scope, $rootScope, TimezoneOptions, GeneralOptions, CompanyStoreService) {
+        'ngInject';
+
+        var ctrl = this;
+
+        ctrl.timezones = [];
+        ctrl.automaticTimezone = false;
+        ctrl.selectedTimezone = null;
+
+        ctrl.$onInit = function() {
+            ctrl.timezones = TimezoneOptions.generateTimezoneList(ctrl.restrictRegion);
+            ctrl.setNewTimezone = setNewTimezone;
+            ctrl.automaticTimezoneToggle = automaticTimezoneToggle;
+            setDefaults();
+        };
+
+        function automaticTimezoneToggle () {
+
+            var tz;
+
+            if (ctrl.automaticTimezone) {
+                tz = moment.tz.guess();
+                ctrl.selectedTimezone = TimezoneOptions.mapTimezoneForDisplay(tz);
+                ctrl.setNewTimezone(tz, true);
+            }
+
+            if (!ctrl.automaticTimezone) {
+                tz = CompanyStoreService.time_zone;
+                ctrl.selectedTimezone = TimezoneOptions.mapTimezoneForDisplay(tz);
+                resetTimezone(tz);
+            }
+
+            $scope.$broadcast('UISelect:CloseSelect');
+
+        }
+
+        function setNewTimezone (timezone) {
+            localStorage.selectedTimezone = GeneralOptions.display_time_zone = timezone;
+            GeneralOptions.custom_time_zone = CompanyStoreService.time_zone !== timezone ? true : false;
+            GeneralOptions.set_time_zone_automatically = setTzAutomatically;
+            moment.tz.setDefault(timezone);
+            $rootScope.$emit('BBTimezoneOptions:timezoneChanged', timezone);
+        }
+
+        function resetTimezone (tz) {
+            localStorage.removeItem('selectedTimezone');
+            GeneralOptions.display_time_zone = null;
+            GeneralOptions.custom_time_zone = GeneralOptions.set_time_zone_automatically = false;
+            moment.tz.setDefault(tz);
+            $rootScope.$emit('BBTimezoneOptions:timezoneChanged', null);
+        }
+
+        function setDefaults () {
+            if (localStorage.selectedTimezone) {
+                ctrl.selectedTimezone = TimezoneOptions.mapTimezoneForDisplay(localStorage.selectedTimezone);
+                ctrl.automaticTimezone = moment.tz.guess() === localStorage.selectedTimezone ? true : false;
+            } else {
+                ctrl.selectedTimezone = TimezoneOptions.mapTimezoneForDisplay(CompanyStoreService.time_zone);
+            }
+        }
+    }
+
+    angular
+        .module('BBAdminDashboard')
+        .component('bbTimezoneOptions', timezoneOptionsComponent);
+
+})();
