adbd4d2666e019bebdeceddb1e18850679e8cf72
damian pudlo <pudlo.damian@gmail.com>
2017-02-20 23:00:15 +0000

    flatten templates & enable travis deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,7 +4,7 @@ echo "TRAVIS BRANCH: $TRAVIS_BRANCH"
 echo "TRAVIS TAG: $TRAVIS_TAG"
 echo "TRAVIS PULL REQUEST: $TRAVIS_PULL_REQUEST"
 
-#if [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
-#  gulp deploy --env=prod
-#fi
+if [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
+  gulp deploy --env=prod
+fi
 
