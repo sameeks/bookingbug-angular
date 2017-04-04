4bd35847fb05de6c72f4cb70a68bd1cd4e7f8e85
damian pudlo <pudlo.damian@gmail.com>
2017-04-04 17:03:13 +0100

    progress

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,48 @@
 const path = require('path');
 const webpack = require('webpack');
-const webpackMerge = require('webpack-merge');
 
-let baseConfig = {
-    target: 'web',
-    plugins: [
-        /*new webpack.optimize.CommonsChunkPlugin({
-         name: 'inline',
-         filename: 'inline.js',
-         minChunks: Infinity
-         }),
-         new webpack.optimize.AggressiveSplittingPlugin({
-         minSize: 5000,
-         maxSize: 10000
-         }),*/
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['es2015'],
-                        plugins: [
-                            ["transform-es2015-classes", {"loose": true}]
-                        ]
+const config = [
+    {
+        entry: {
+            core: path.resolve(__dirname, 'src/admin/javascripts/main.js'),
+            admin: path.resolve(__dirname, 'src/admin/javascripts/main.js'),
+            member: path.resolve(__dirname, 'src/admin/javascripts/main.js'),
+            'public-booking': path.resolve(__dirname, 'src/admin/javascripts/main.js')
+        },
+        output: {
+            path: path.resolve(__dirname, 'build-new'),
+            filename: '[name]/bookingbug-angular-[name].js'
+        },
+        target: 'web',
+        plugins: [
+            /*new webpack.optimize.CommonsChunkPlugin({
+             name: 'inline',
+             filename: 'inline.js',
+             minChunks: Infinity
+             }),
+             new webpack.optimize.AggressiveSplittingPlugin({
+             minSize: 5000,
+             maxSize: 10000
+             }),*/
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015'],
+                            plugins: [
+                                ["transform-es2015-classes", {"loose": true}]
+                            ]
+                        }
                     }
                 }
-            }
-        ]
-    }
-};
-
-let bbModules = ['admin', 'admin-booking', 'admin-dashboard', 'core', 'events', 'member', 'public-booking', 'services', 'settings']
-    .map(
-        (bbModule) => {
-            return webpackMerge(baseConfig, {
-                entry: {
-                    entry: path.resolve(__dirname, 'src/' + bbModule + '/javascripts/main.js')
-                },
-                output: {
-                    path: path.resolve(__dirname, 'build-new/' + bbModule),
-                    filename: 'bookingbug-angular-' + bbModule + '.js'
-                }
-            });
+            ]
         }
-    );
+    }
+];
 
-module.exports = bbModules;
-
-
-
+module.exports = config;
