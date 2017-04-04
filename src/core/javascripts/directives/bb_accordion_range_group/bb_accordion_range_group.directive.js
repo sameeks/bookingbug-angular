db7ec7a1b342969fc3d20fb5202b320fecb4d65e
Terry <terrancemutizwa@yahoo.com>
2017-04-04 12:47:35 +0100

    Keyboard friendly calendar
    improve - Keyboard friendly calendar

--- src/core/javascripts/directives/bb_accordion_range_group/bb_accordion_range_group.directive.js
+++ src/core/javascripts/directives/bb_accordion_range_group/bb_accordion_range_group.directive.js
@@ -30,22 +30,46 @@
 
 
 angular.module('BB.Directives').directive('bbAccordionRangeGroup', PathSvc => {
-        return {
-            restrict: 'AE',
-            replace: false,
-            scope: {
-                day: '=',
-                slots: '=',
-                selectSlot: '=',
-                disabled_slot: "=disabledSlot"
-            },
-            controller: 'AccordionRangeGroup',
-            link(scope, element, attrs) {
-                return scope.options = scope.$eval(attrs.bbAccordionRangeGroup) || {};
-            },
-            templateUrl(element, attrs) {
-                return PathSvc.directivePartial("_accordion_range_group");
-            }
-        };
-    }
+    return {
+        restrict: 'AE',
+        replace: false,
+        scope: {
+            day: '=',
+            slots: '=',
+            selectSlot: '=',
+            disabled_slot: "=disabledSlot"
+        },
+        controller: 'AccordionRangeGroup',
+        link(scope, elem, attrs) {
+            let nextElem, next = 0; //the next element and counter/position
+            let numChildren = 0; //the number of slots available
+            //capture enter and arrow keys to navigate slot
+            elem.bind('keydown', (e) => {
+                //on key enter
+                if ((e.keyCode === 13) || (e.keyCode === 32)) {
+                    scope.is_open = !scope.is_open; //toggle accordion
+                    numChildren = elem.find('#time-slots li').length;
+                }
+                //if up and down arrows pressed and accordion group open, cycle focus through the options
+                if (!!scope.is_open && numChildren > 0) {
+                    if (e.keyCode === 40) {
+                        next++;
+                        next = (next > numChildren)?1:next;
+                    }
+                    if(e.keyCode === 38){
+                        next--;
+                        next = (next < 1)?numChildren:next;
+                    }
+                    nextElem = elem.find('#time-slots li:nth-child(' + next + ')');
+                    nextElem.focus();
+                }
+                scope.$apply();
+            });
+            return scope.options = scope.$eval(attrs.bbAccordionRangeGroup) || {};
+        },
+        templateUrl(elem, attrs) {
+            return PathSvc.directivePartial("_accordion_range_group");
+        }
+    };
+}
 );
