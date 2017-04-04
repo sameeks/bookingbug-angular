9800d391ca80ffe159dd433a7bfb54c06edb24b9
Carlos Rios <crios@bookingbug.com>
2017-04-04 12:49:50 +0100

    sorting out timezone names'

--- src/core/javascripts/i18n/directives/bb_time_zone_options.component.js
+++ src/core/javascripts/i18n/directives/bb_time_zone_options.component.js
@@ -7,28 +7,30 @@
      * @name BBAdminDashboard.bbTimeZoneOptions
      *
      * @description
-     * TimeZone options
+     * TimeZone Select
      *
      * @example
      * <example>
-     *   <bb-time-zone-options restrict-region="'Europe'"></bb-time-zone-options>
-     *   <bb-time-zone-options restrict-region="['Asia', 'America']"></bb-time-zone-options>
+     *   <bb-time-zone-select restrict-region="'Europe'"></bb-time-zone-select>
+     *   <bb-time-zone-select restrict-region="['Asia', 'America']"></bb-time-zone-select>
      * </example>
      */
 
     angular
         .module('BB.i18n')
-        .component('bbTimeZoneOptions', {
-            templateUrl: 'i18n/_bb_timezone_options.html',
+        .component('bbTimeZoneSelect', {
+            templateUrl: 'i18n/_bb_timezone_select.html',
             bindings: {
-                restrictRegion: '<',
-                options: '<'
+                momentNames: '<',
+                limitTo: '<',
+                exclude: '<',
+                format: '<'
             },
-            controller: TimeZoneOptionsCtrl,
-            controllerAs: '$bbTimeZoneOptionsCtrl'
+            controller: TimeZoneSelectCtrl,
+            controllerAs: '$bbTimeZoneSelectCtrl'
         });
 
-    function TimeZoneOptionsCtrl($scope, $rootScope, bbTimeZone, bbTimeZoneOptions, CompanyStoreService, bbi18nOptions, $localStorage) {
+    function TimeZoneSelectCtrl($scope, $rootScope, bbTimeZone, bbTimeZoneOptions, CompanyStoreService, bbi18nOptions, $localStorage) {
         'ngInject';
 
         const ctrl = this;
@@ -38,7 +40,7 @@
         ctrl.selectedTimeZone = null;
 
         ctrl.$onInit = function () {
-            ctrl.timeZones = bbTimeZoneOptions.generateTimeZoneList(ctrl.restrictRegion, ctrl.options); //TODO should be more customisable
+            ctrl.timeZones = bbTimeZoneOptions.generateTimeZoneList(ctrl.momentNames, ctrl.limitTo, ctrl.exclude, ctrl.format); //TODO should be more customisable
             ctrl.updateTimeZone = updateTimeZone;
             ctrl.automaticTimeZoneToggle = automaticTimeZoneToggle;
             if (bbi18nOptions.use_browser_time_zone && $localStorage.getItem('bbTimeZone') === undefined) ctrl.isAutomaticTimeZone = true;
@@ -47,7 +49,7 @@
 
         function determineTimeZone() {
             bbTimeZone.determineTimeZone();
-            ctrl.selectedTimeZone = bbTimeZoneOptions.mapTimeZoneItem(bbTimeZone.getDisplayTimeZone());
+            ctrl.selectedTimeZone = _.find(ctrl.timeZones, (timeZone) => timeZone.value === bbTimeZone.getDisplayTimeZone());
         }
 
         function automaticTimeZoneToggle() {
--- src/core/javascripts/i18n/services/bb_time_zone_options.service.js
+++ src/core/javascripts/i18n/services/bb_time_zone_options.service.js
@@ -17,42 +17,129 @@
             generateTimeZoneList: generateTimeZoneList
         };
 
-        function loadTimeZones () {
-            let locationNames = moment.tz.names();
-            locationNames = _.chain(locationNames)
-                .filter((tz) => tz.indexOf('GMT') === -1)
-                .filter((tz) => tz.indexOf('Etc') === -1)
-                .filter((tz) => tz.match(/[^/]*$/)[0] !== tz.match(/[^/]*$/)[0].toUpperCase())
-                .value();
-            return locationNames;
+        function loadTimeZones (momentNames) {
+            let locationNames = [];
+
+            if (momentNames) {
+                locationNames = moment.tz.names();
+                locationNames = _.chain(locationNames)
+                    .filter((tz) => tz.indexOf('GMT') === -1)
+                    .filter((tz) => tz.indexOf('Etc') === -1)
+                    .filter((tz) => tz.match(/[^/]*$/)[0] !== tz.match(/[^/]*$/)[0].toUpperCase())
+                    .value();
+                return locationNames;
+            }
+
+            if (!momentNames) {
+                locationNames = ['Etc/GMT+12',
+                    'Pacific/Pago_Pago',
+                    'Pacific/Honolulu',
+                    'US/Alaska',
+                    'America/Los_Angeles',
+                    'America/Denver',
+                    'America/Chihuahua',
+                    'America/Phoenix',
+                    'America/Chicago',
+                    'Canada/Saskatchewan',
+                    'America/Mexico_City',
+                    'America/El_Salvador',
+                    'America/New_York',
+                    'America/Indiana/Knox',
+                    'America/Bogota',
+                    'America/Halifax',
+                    'America/Caracas',
+                    'America/Santiago',
+                    'Canada/Newfoundland',
+                    'America/Sao_Paulo',
+                    'America/Buenos_Aires',
+                    'America/Thule',
+                    'Atlantic/Azores',
+                    'Atlantic/Cape_Verde',
+                    'Europe/London',
+                    'Africa/Casablanca',
+                    'Europe/Belgrade',
+                    'Europe/Sarajevo',
+                    'Europe/Paris',
+                    'Europe/Amsterdam',
+                    'Africa/Lagos',
+                    'Europe/Bucharest',
+                    'Africa/Cairo',
+                    'Europe/Helsinki',
+                    'Europe/Athens',
+                    'Asia/Jerusalem',
+                    'Africa/Harare',
+                    'Europe/Istanbul',
+                    'Europe/Moscow',
+                    'Asia/Kuwait',
+                    'Africa/Nairobi',
+                    'Asia/Baghdad',
+                    'Asia/Tehran',
+                    'Asia/Dubai',
+                    'Asia/Yerevan',
+                    'Asia/Kabul',
+                    'Asia/Yekaterinburg',
+                    'Asia/Karachi',
+                    'Asia/Kolkata',
+                    'Asia/Kathmandu',
+                    'Asia/Dhaka',
+                    'Asia/Colombo',
+                    'Asia/Almaty',
+                    'Asia/Rangoon',
+                    'Asia/Bangkok',
+                    'Asia/Krasnoyarsk',
+                    'Asia/Hong_Kong',
+                    'Asia/Singapore',
+                    'Asia/Taipei',
+                    'Australia/Perth',
+                    'Asia/Irkutsk',
+                    'Asia/Seoul',
+                    'Asia/Tokyo',
+                    'Asia/Yakutsk',
+                    'Australia/Darwin',
+                    'Australia/Adelaide',
+                    'Australia/Canberra',
+                    'Australia/Brisbane',
+                    'Australia/Tasmania',
+                    'Asia/Vladivostok',
+                    'Pacific/Guam',
+                    'Asia/Magadan',
+                    'Pacific/Fiji',
+                    'Pacific/Auckland',
+                    'Pacific/Tongatapu'];
+                return locationNames;
+            }
         }
 
-        function restrictToRegion (timeZones, restrictRegion) {
+        function updateTimeZoneNames (timeZones, timeZone, exclude) {
 
-            function filterTimeZones (filterBy) {
-                return _.filter(timeZones, (tz) => tz.indexOf(filterBy) !== -1);
+            if (angular.isString(timeZone)) {
+                return timeZoneFilter(timeZone);
             }
 
-            if (angular.isString(restrictRegion)) {
-                return filterTimeZones(restrictRegion);
-            }
-
-            if (angular.isArray(restrictRegion)) {
+            if (angular.isArray(timeZone)) {
                 let locations = [];
-                _.each(restrictRegion, (region) => locations.push(filterTimeZones(region)));
+                _.each(timeZone, (region) => locations.push(timeZoneFilter(region)));
                 return _.flatten(locations);
             }
 
             return timeZones;
+
+            function timeZoneFilter (filterBy) {
+                if (exclude) {
+                    return _.reject(timeZones, (tz) => tz.indexOf(filterBy) !== -1);
+                } else {
+                    return _.filter(timeZones, (tz) => tz.indexOf(filterBy) !== -1);
+                }
+            }
         }
 
-        function formatDisplayValue ({translate = true, format} = {}, city, momentTz) {
-            let display;
+        function formatDisplayValue (format, city, momentTz) {
+            let display = '';
+
             const formatMap = {
-                'utc-code': 'UTC',
-                'tz-code': translate ? $translate.instant(`COMMON.TIME_ZONE_CODES.${momentTz.format('zz')}`) : momentTz.format('zz'),
+                'tz-code': $translate.instant(`COMMON.TIME_ZONE_CODES.${momentTz.format('zz')}`),
                 'offset-hours': momentTz.format('Z'),
-                'location': translate ? $translate.instant(`COMMON.LOCATIONS.${city}`) : city
+                'location': $translate.instant(`COMMON.LOCATIONS.${city}`)
             };
 
             if (format && angular.isString(format)) {
@@ -60,7 +147,7 @@
                 return display;
             }
 
-            display = `(UTC ${momentTz.format('Z')}) ${formatMap['location']} (${formatMap['tz-code']})`;
+            display = `(GMT${formatMap['offset-hours']}) ${formatMap['location']}`;
             return display;
         }
 
@@ -72,13 +159,13 @@
         * @param {Integer} Index
         * @returns {Object} A time zone oject
         */
-        function mapTimeZoneItem (location, index, options) {
+        function mapTimeZoneItem (location, index, format) {
             const timeZone = {};
             const city = location.match(/[^/]*$/)[0].replace(/-/g, '_').toUpperCase();
             const momentTz = moment.tz(location);
-            timeZone.display = formatDisplayValue(options, city, momentTz);
+            timeZone.display = formatDisplayValue(format, city, momentTz);
             timeZone.value = location;
-            if (index) {
+            if (angular.isNumber(index)) {
                 timeZone.id = index;
                 timeZone.order = [parseInt(momentTz.format('Z')), momentTz.format('zz'), city];
             }
@@ -92,12 +179,21 @@
         * @param {String, Array} Restrict the time zones to one region (String) or multiple regions (Array)
         * @returns {Array} A list of time zones
         */
-        function generateTimeZoneList (restrictRegion, options) {
+        function generateTimeZoneList (momentNames, limitTo, exclude, format) {
             let timeZones = [];
 
-            const timeZoneNames = restrictToRegion(loadTimeZones(), restrictRegion);
+            let timeZoneNames = loadTimeZones(momentNames);
+
+            if (limitTo) {
+                timeZoneNames = updateTimeZoneNames(timeZoneNames, limitTo);
+            }
+
+            if (exclude) {
+                timeZoneNames = updateTimeZoneNames(timeZoneNames, exclude, true);
+            }
+
             for (let [index, value] of timeZoneNames.entries()) {
-                timeZones.push(mapTimeZoneItem(value, index, options));
+                timeZones.push(mapTimeZoneItem(value, index, format));
             }
 
             timeZones = _.uniq(timeZones, (timeZone) => timeZone.display);
