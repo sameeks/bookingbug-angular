aab8c878c5f44039140aa2a526a739c70eef9d07
Mark Shakespeare <mshakespeare@bookingbug.com>
2017-04-05 14:22:00 +0100

    move grid filter method into bbGridService

--- src/admin-dashboard/javascripts/clients/directives/bb_clients_table.directive.js
+++ src/admin-dashboard/javascripts/clients/directives/bb_clients_table.directive.js
@@ -74,25 +74,8 @@ function BBClientsTable(bbGridService) {
         });
 
         let buildFilterString = (filters) => {
-            let filterItems = [];
-            let filterString = '';
             // we need to build a string in format "field,value,field,value,field,value"
-            for(let filter of filters) {
-                filter.string = filter.fieldName + ',' + filter.value;
-            }
-
-            for(let filter of filters) {
-                if(filters.length === 1) {
-                    filterString = filter.string;
-                } else {
-                    filterString = filterString + ',' + filter.string;
-                }
-            }
-
-            if(filterString.charAt(0) === ',')  {
-                filterString = filterString.substr(1);
-            }
-
+            let filterString = bbGridService.formatFilterString(filters);
             scope.getClients(scope.paginationOptions.pageNumber, filterString);
         }
 
--- src/admin-dashboard/javascripts/core/services/bb_grid.service.js
+++ src/admin-dashboard/javascripts/core/services/bb_grid.service.js
@@ -13,6 +13,28 @@ function bbGridService() {
             }
 
             return columns;
+        },
+
+        formatFilterString(filters) {
+             // we need to build a string in format "field,value,field,value,field,value"
+            let filterString = '';
+            for(let filter of filters) {
+                filter.string = filter.fieldName + ',' + filter.value;
+            }
+
+            for(let filter of filters) {
+                if(filters.length === 1) {
+                    filterString = filter.string;
+                } else {
+                    filterString = filterString + ',' + filter.string;
+                }
+            }
+
+            if(filterString.charAt(0) === ',')  {
+                filterString = filterString.substr(1);
+            }
+
+            return filterString;
         }
     }
 }
