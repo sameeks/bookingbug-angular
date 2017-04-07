3e55909b3862111ee56b09dbd59b11fde82d4703
Mark Shakespeare <mshakespeare@bookingbug.com>
2017-04-07 16:41:22 +0100

    fix select first slot directive and translation

--- src/admin-booking/javascripts/directives/select_first_slot.js
+++ src/admin-booking/javascripts/directives/select_first_slot.js
@@ -1,33 +1,44 @@
-angular.module('BB.Directives').directive('selectFirstSlot', () => {
-        return {
-            link(scope, el, attrs) {
-                return scope.$on('slotsUpdated', function (e, basket_item, slots) {
-                    // -------------------------------------
-                    // Only show TimeSlots in the future!
-                    // -------------------------------------
-                    slots = _.filter(slots, slot => slot.time_moment.isAfter(moment()));
-                    // --------------------------------------
-                    // Select the first available TimeSlot
-                    // --------------------------------------
-                    if (slots[0]) {
-                        scope.bb.selected_slot = slots[0];
-                        scope.bb.selected_date = scope.selected_date;
-                        let hours = slots[0].time_24.split(":")[0];
-                        let minutes = slots[0].time_24.split(":")[1];
-                        scope.bb.selected_date.hour(hours).minutes(minutes).seconds(0);
-                        scope.highlightSlot(slots[0], scope.selected_day);
-                    }
-                    return (() => {
-                        let result = [];
-                        for (let slot of Array.from(slots)) {
-                            if (!slot.selected) {
-                                result.push(slot.hidden = true);
-                            }
-                        }
-                        return result;
-                    })();
-                });
-            }
-        };
+angular
+    .module('BB.Directives')
+    .directive('selectFirstSlot', bbSelectFirstSlot);
+
+function bbSelectFirstSlot() {
+    let directive = {
+        link
     }
-);
+
+    return directive;
+
+    function link(scope, elem, attrs) {
+        scope.$on('slotsUpdated', (e, basket_item, slots) => {
+            let timeNow = moment();
+            // -------------------------------------
+            // Only show TimeSlots in the future!
+            // -------------------------------------
+            slots = _.filter(slots, slot => slot.datetime.isAfter(timeNow));
+            // --------------------------------------
+            // Select the first available TimeSlot
+            // --------------------------------------
+            if (slots[0]) {
+                let firstSlot = slots[0];
+
+                scope.bb.selected_slot = firstSlot;
+                scope.bb.selected_date = scope.selected_date;
+                let hours = moment(firstSlot.datetime).format("HH");
+                let minutes = moment(firstSlot.datetime).format("mm");
+                scope.bb.selected_date.hour(hours).minutes(minutes).seconds(0);
+                scope.highlightSlot(firstSlot, scope.selected_day);
+            }
+            return (() => {
+                let result = [];
+                for (let slot of Array.from(slots)) {
+                    if (!slot.selected) {
+                        result.push(slot.hidden = true);
+                    }
+                }
+                return result;
+            })();
+        });
+    }
+}
+
