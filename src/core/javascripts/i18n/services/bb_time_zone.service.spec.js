9e4fef330cb87d250e5ce6195b3d935047fa1fb3
damian pudlo <pudlo.damian@gmail.com>
2017-04-10 18:02:17 +0100

    bbTimeZone unit tests progress

--- src/core/javascripts/i18n/services/bb_time_zone.service.spec.js
+++ src/core/javascripts/i18n/services/bb_time_zone.service.spec.js
@@ -1,20 +1,24 @@
-fdescribe('bbTimeZone service,', () => {
+describe('bbTimeZone service,', () => {
 
     let bbTimeZone = null;
     let moment = null;
 
-    const setBbi18nOptions = (options) => {
+    const setBbi18nOptions = (options = {}) => {
         module(function ($provide) {
             $provide.provider('bbi18nOptions', function () {
                 this.$get = () => {
-                    return options;
+                    return Object.assign({
+                        available_languages: ['en'],
+                        default_time_zone: 'Europe/London',
+                        use_browser_time_zone: false,
+                        use_company_time_zone: false
+                    }, options);
                 };
             });
 
             $provide.factory('CompanyStoreService', function () {
                 return {time_zone: 'America/New_York'};
             });
-
         });
 
         inject(($injector) => {
@@ -28,15 +32,18 @@ fdescribe('bbTimeZone service,', () => {
         module('BB.i18n');
     });
 
+    beforeEach(() => {
+        localStorage.clear();
+    });
+
     describe('when calling "determine"', () => {
 
+        afterEach(() => {
+            localStorage.clear();
+        });
+
         it('should use default timezone', () => {
-            setBbi18nOptions({
-                available_languages: ['en'],
-                default_time_zone: 'Europe/London',
-                use_browser_time_zone: false,
-                use_company_time_zone: false
-            });
+            setBbi18nOptions();
 
             bbTimeZone.determineTimeZone();
 
@@ -45,10 +52,7 @@ fdescribe('bbTimeZone service,', () => {
 
         it('should use browser timezone', () => {
             setBbi18nOptions({
-                available_languages: ['en'],
-                default_time_zone: 'Europe/London',
-                use_browser_time_zone: true,
-                use_company_time_zone: false
+                use_browser_time_zone: true
             });
 
             spyOn(moment.tz, 'guess').and.returnValue('Europe/Berlin');
@@ -60,9 +64,6 @@ fdescribe('bbTimeZone service,', () => {
 
         it('should use company timezone', () => {
             setBbi18nOptions({
-                available_languages: ['en'],
-                default_time_zone: 'Europe/London',
-                use_browser_time_zone: false,
                 use_company_time_zone: true
             });
             bbTimeZone.determineTimeZone();
@@ -70,15 +71,46 @@ fdescribe('bbTimeZone service,', () => {
         });
 
         it('should use browser timezone (use_browser_time_zone has precedence over use_company_time_zone)', () => {
+
             setBbi18nOptions({
-                available_languages: ['en'],
-                default_time_zone: 'Europe/London',
                 use_browser_time_zone: true,
                 use_company_time_zone: true
             });
-            spyOn(moment.tz, 'guess').and.returnValue('Europe/Berlin');
+
+            spyOn(moment.tz, 'guess').and.returnValue('Europe/Paris');
+
             bbTimeZone.determineTimeZone();
-            expect(bbTimeZone.getDisplayTimeZone()).toBe('Europe/Berlin');
+
+            expect(bbTimeZone.getDisplayTimeZone()).toBe('Europe/Paris');
         });
+
+        it('should use time zone stored in browser local storage', () => {
+            setBbi18nOptions({
+                use_browser_time_zone: true,
+                use_company_time_zone: true
+            });
+
+            bbTimeZone.setLocalStorage({displayTimeZone: 'Canada/Mountain'});
+            bbTimeZone.determineTimeZone();
+
+            expect(bbTimeZone.getDisplayTimeZone()).toBe('Canada/Mountain');
+        });
+
+        it('should use browser timezone (enforced by value in browser local storage)', () => {
+
+            setBbi18nOptions({
+                use_browser_time_zone: false,
+                use_company_time_zone: false
+            });
+
+            spyOn(moment.tz, 'guess').and.returnValue('Europe/Paris');
+
+            bbTimeZone.setLocalStorage({useBrowserTimeZone: true});
+            bbTimeZone.determineTimeZone();
+
+            expect(bbTimeZone.getDisplayTimeZone()).toBe('Europe/Paris');
+        });
+
+
     });
 });
